92985d9dceae9d55ae958b24474d1bba21aa9532
Jay <Jayakumar8263@gmail.com>
2025-08-27 18:03:14 +0000

    can you create a deploy pages actions for github pages to setup this pro

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -29,15 +31,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/contact',
-        destination: '/contact-5',
-        permanent: true,
-      }
-    ]
   },
 };
 
